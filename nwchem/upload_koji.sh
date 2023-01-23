33a7e5b4f32ff7cb518e340f7995500b3eda4a9f
edoapra <edoardo.apra@gmail.com>
2023-01-22 17:56:48 -0800

    update

--- nwchem/upload_koji.sh
+++ nwchem/upload_koji.sh
@@ -6,8 +6,8 @@ rpmbuild --buildroot $RPMBUILD_DIR --undefine=_disable_source_fetch -bs nwchem.s
 export KRB5_CONFIG=/etc/krb5.conf.fedora
 NWVER=7.2.0-1
 kinit edoapra@FEDORAPROJECT.ORG
-timeout --preserve-status --kill-after 10 50 koji build --scratch epel7 ~/rpmbuild/SRPMS/nwchem-$NWVER.src.rpm 
-#timeout --preserve-status --kill-after 10 50 koji build --scratch epel8 ~/rpmbuild/SRPMS/nwchem-$NWVER.src.rpm  
-#timeout --preserve-status --kill-after 10 50 koji build --scratch f32 ~/rpmbuild/SRPMS/nwchem-$NWVER.src.rpm  
-#timeout --preserve-status --kill-after 10 50 koji build --scratch f33 ~/rpmbuild/SRPMS/nwchem-$NWVER.src.rpm  
 #timeout --preserve-status --kill-after 10 50 koji build --scratch rawhide ~/rpmbuild/SRPMS/nwchem-$NWVER.src.rpm 
+timeout --preserve-status --kill-after 10 50 koji build --scratch epel7 ~/rpmbuild/SRPMS/nwchem-$NWVER.src.rpm 
+timeout --preserve-status --kill-after 10 50 koji build --scratch epel8 ~/rpmbuild/SRPMS/nwchem-$NWVER.src.rpm  
+timeout --preserve-status --kill-after 10 50 koji build --scratch f36 ~/rpmbuild/SRPMS/nwchem-$NWVER.src.rpm  
+timeout --preserve-status --kill-after 10 50 koji build --scratch f37 ~/rpmbuild/SRPMS/nwchem-$NWVER.src.rpm  
